0832872d234ef3b3276066057e49fc84d578a1f9
Nate-Workspace <nathanisrael.jobs@gmail.com>
2025-02-18 03:48:52 +0300

    Adding the form functionality

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,47 @@
-"use client"
+"use client";
 
-import { Button, TextArea, TextField } from "@radix-ui/themes"
-import SimpleMDE from "react-simplemde-editor";
+import { Button, TextArea, TextField } from "@radix-ui/themes";
+import dynamic from "next/dynamic";
 import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
-const newIssuePage = () => {
-  return (
-    <div className="max-w-xl space-y-3">
-        <TextField.Root placeholder="Title" />
-        <SimpleMDE placeholder="write a description"/>
-        <Button>Add an issue</Button>
-    </div>
-  )
+// Disable SSR for SimpleMDE
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+
+interface formType {
+  title: string;
+  description: string;
 }
 
-export default newIssuePage
+const newIssuePage = () => {
+  const { register, control, handleSubmit } = useForm<formType>();
+  const router= useRouter();
+
+  const submitListener= async (data: formType)=>{
+    await axios.post("/api/issues", data)
+    router.push("/issues")
+}
+
+  return (
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(submitListener)}
+    >
+      <TextField.Root placeholder="Title" {...register("title")} />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="write a description" {...field} />
+        )}
+      />
+      <Button>Add an issue</Button>
+    </form>
+  );
+};
+
+export default newIssuePage;
