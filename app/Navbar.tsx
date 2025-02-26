eb163cdee8cead84460102555f8815455224036f
Nate-Workspace <nathanisrael.jobs@gmail.com>
2025-02-26 17:38:12 +0300

    Navbar UI edit

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -5,7 +5,7 @@ import { FaBug } from "react-icons/fa";
 import classNames from "classnames";
 import { usePathname } from "next/navigation";
 import { useSession } from "next-auth/react";
-import { Box } from "@radix-ui/themes";
+import { Box, Container, Flex } from "@radix-ui/themes";
 
 const Navbar = () => {
   const links = [
@@ -16,32 +16,42 @@ const Navbar = () => {
   const { status, data: session } = useSession();
   console.log(currentPath);
   return (
-    <nav className="flex px-5 space-x-6 mb-4 items-center border-b h-14">
-      <Link href="/">
-        <FaBug />
-      </Link>
-      <ul className="flex px-4 space-x-14">
-        {links.map((each) => (
-          <li key={each.href}>
-            <Link
-              href={each.href}
-              className={classNames({
-                "text-zinc-500": each.href !== currentPath,
-                "text-zinc-900": each.href == currentPath,
-                "hover:text-zinc-800 transition-colors hover:cursor-pointer":
-                  true,
-              })}
-            >
-              {each.label}
+    <nav className="px-5 mb-4 border-b py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <FaBug />
             </Link>
-          </li>
-        ))}
-      </ul>
+            <ul className="flex px-4 space-x-14">
+              {links.map((each) => (
+                <li key={each.href}>
+                  <Link
+                    href={each.href}
+                    className={classNames({
+                      "text-zinc-500": each.href !== currentPath,
+                      "text-zinc-900": each.href == currentPath,
+                      "hover:text-zinc-800 transition-colors hover:cursor-pointer":
+                        true,
+                    })}
+                  >
+                    {each.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
 
-      <Box>
-        {status=== "authenticated" && (<Link href='/api/auth/signout'>Logout</Link>)}
-        {status === "unauthenticated" && (<Link href='/api/auth/signin'>Login</Link>)}
-      </Box>
+          <Box>
+            {status === "authenticated" && (
+              <Link href="/api/auth/signout">Logout</Link>
+            )}
+            {status === "unauthenticated" && (
+              <Link href="/api/auth/signin">Login</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };
