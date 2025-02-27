92cec52d7273ff02a80bc4d20e596d1308fd6191
Nate-Workspace <nathanisrael.jobs@gmail.com>
2025-02-28 02:17:11 +0300

    Adding a Notificatino toaster

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,39 +1,57 @@
-'use client'
+"use client";
 
-import { Issue, User } from '@prisma/client'
-import { Select } from '@radix-ui/themes'
-import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import {Skeleton} from '@/app/components'
+import { Issue, User } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Skeleton } from "@/app/components";
+import toast, { Toaster } from "react-hot-toast";
 
-const AssigneeSelect = ({issue}: {issue: Issue}) => {
-    const { data:users , error , isLoading } =useQuery<User[]>({         // The error and isLoading are provided by tanstack Just like we do manually in useState and try catch
-        queryKey: ['users'],
-        queryFn: ()=> axios.get('/api/users').then(res=>res.data),
-        staleTime: 60 * 1000,  // The list of data will be treated as fresh for 60 seconds( Won't get re featched for 60 seconds)
-        retry: 3  // If the fetch failed, react will retry the fetch up to 3 additional times( 4 total)
-    })
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    // The error and isLoading are provided by tanstack Just like we do manually in useState and try catch
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000, // The list of data will be treated as fresh for 60 seconds( Won't get re featched for 60 seconds)
+    retry: 3, // If the fetch failed, react will retry the fetch up to 3 additional times( 4 total)
+  });
 
-    if(isLoading) return <Skeleton/>
-    if(error) return null;
-    
+  if (isLoading) return <Skeleton />;
+  if (error) return null;
+
   return (
-    <Select.Root defaultValue={issue.assignedToUserId || "unassigned"} onValueChange={(userID)=>{
-        axios.patch(`/api/issues/${issue.id}`, {assignedToUserId: userID=== "unassigned"? null : userID})
-    }}>
-        <Select.Trigger placeholder='Assign...'/>
+    <>
+      <Select.Root
+        defaultValue={issue.assignedToUserId || "unassigned"}
+        onValueChange={(userID) => {
+          axios.patch(`/api/issues/${issue.id}`, {
+            assignedToUserId: userID === "unassigned" ? null : userID,
+          }).catch(()=>{
+            toast.error("Couldn't make the change")
+          });
+        }}
+      >
+        <Select.Trigger placeholder="Assign..." />
         <Select.Content>
-            <Select.Group>
-                <Select.Label>Suggestions</Select.Label>
-                <Select.Item value="unassigned">Unassigned</Select.Item>
-                {users?.map(user=> 
-                (<Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>)
-                )}
-            </Select.Group>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value="unassigned">Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
         </Select.Content>
-    </Select.Root>
-  )
-}
+      </Select.Root>
+      <Toaster />
+    </>
+  );
+};
 
-export default AssigneeSelect
+export default AssigneeSelect;
