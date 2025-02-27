5f2435178e2afe23201b0c4fd0f7cccb497e5b53
Nate-Workspace <nathanisrael.jobs@gmail.com>
2025-02-28 02:05:54 +0300

    Assign an issue to a user

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import { User } from '@prisma/client'
+import { Issue, User } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import {Skeleton} from '@/app/components'
 
-const AssigneeSelect = () => {
+const AssigneeSelect = ({issue}: {issue: Issue}) => {
     const { data:users , error , isLoading } =useQuery<User[]>({         // The error and isLoading are provided by tanstack Just like we do manually in useState and try catch
         queryKey: ['users'],
         queryFn: ()=> axios.get('/api/users').then(res=>res.data),
@@ -19,11 +19,14 @@ const AssigneeSelect = () => {
     if(error) return null;
     
   return (
-    <Select.Root>
+    <Select.Root defaultValue={issue.assignedToUserId || "unassigned"} onValueChange={(userID)=>{
+        axios.patch(`/api/issues/${issue.id}`, {assignedToUserId: userID=== "unassigned"? null : userID})
+    }}>
         <Select.Trigger placeholder='Assign...'/>
         <Select.Content>
             <Select.Group>
                 <Select.Label>Suggestions</Select.Label>
+                <Select.Item value="unassigned">Unassigned</Select.Item>
                 {users?.map(user=> 
                 (<Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>)
                 )}
